c2c6542fdb284d84c62619fd5168f5ba21b16b19
Peter <The Beast v2.0@10.0.0.32>
2021-02-07 15:43:14 -0500

    Added name to authors

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -12,6 +12,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * When you create your own branch, add your name as a modifier.
  *
  * @author dancye & Peter Wyroba
+ * Modified Feb. 7, 2021
  */
 public class Card {
 
